9a735d8a67c714c5d2975ed317db344befee4a3c
mattbillard <mattbillarddesign@gmail.com>
2017-03-02 11:47:45 -0500

    Server: CORS so the Angular2 and React apps can XHR to the server running on a different port

--- server-api/app.js
+++ server-api/app.js
@@ -15,6 +15,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
+// CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
