6d3593b6f53a6e4f2b124cb1b3d05d76b43ddcc0
Matt Billard <mattbillarddesign@gmail.com>
2017-12-29 12:44:07 -0500

    Spring Boot: enable CORS and change port to 3000

---
+++ spring-boot/src/main/java/sample/flyway/MyConfiguration.java
@@ -0,0 +1,23 @@
+package sample.flyway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+
+        		// Enable CORS
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
